90e1fb38cfd0e08f832639c47b75464b22b54992
Demius Academius <demius@acc.md>
2019-03-06 09:33:43 +0200

    initial daemonset manifest for image-builder

--- content/scripts/push-image-builder.sh
+++ content/scripts/push-image-builder.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-microk8s.docker build -t image-builder.aria applications/System/image-builder
+microk8s.docker build -t localhost:32000/image-builder.aria:0.0.1 applications/System/image-builder
+microk8s.docker push localhost:32000/image-builder.aria:0.0.1
