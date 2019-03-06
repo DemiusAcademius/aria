4bbde5cc58c660e1ceed17b5220826b4d97306f1
Demius Academius <demius@acc.md>
2019-03-06 11:51:27 +0200

    tweaks for local registry

--- content/scripts/push-image-builder.sh
+++ content/scripts/push-image-builder.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-microk8s.docker build -t localhost:32000/image-builder.aria:0.0.1 applications/System/image-builder
-microk8s.docker push localhost:32000/image-builder.aria:0.0.1
+docker build -t image-builder.aria:0.0.1 applications/System/image-builder
+docker tag image-builder.aria:0.0.1 127.0.0.1:32000/image-builder.aria:0.0.1
+docker push 127.0.0.1:32000/image-builder.aria:0.0.1
