f9d738baa97c83c387623fe7210e265342fcb2bb
Demius Academius <demius@acc.md>
2019-03-21 14:53:48 +0200

    tuning of install scripts

--- content/scripts/install.sh
+++ content/scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BLUE='\033[1;34m'
+BLUE='\033[1;36m'
 RED='\033[1;31m'
 NC='\033[0m' # No Color
 
@@ -23,6 +23,11 @@ apt update & apt upgrade -y
 sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
 swapoff -a
 
+# copy certificates
+cp certs/acc.io/*.crt /usr/local/share/ca-certificates
+cp certs/acc.md/*.crt /usr/local/share/ca-certificates
+update-ca-certificates
+
 # install docker
 echo -e "${BLUE}INSTALL DOCKER${NC}"
 apt-get install docker.io -y
@@ -41,5 +46,17 @@ sysctl net.bridge.bridge-nf-call-iptables=1
 # run installation
 echo -e "${BLUE}INSTALL KUBERNETES${NC}"
 kubeadm init --pod-network-cidr=10.244.0.0/16
+
+echo -e "${RED}  COPY AND SAVE THE ABOVE!${NC}"
+echo ""
+
+# post install
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown -R acc-server-admin:acc-server-admin $HOME/.kube/config
+
+# install pod network addon (flannel)
+# see: https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
 
 echo -e "${RED}OK${NC}"
--- content/scripts/post-install.sh
+++ content/scripts/post-install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-BLUE='\033[1;34m'
+BLUE='\033[1;36m'
 RED='\033[1;31m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}POST INSTALL${NC}"
 
-# Create dashboard, dashboard user && namespaces
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
+# master isolation for single node installation
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+# create dashboard, dashboard user && namespaces
+# SEE: https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
+echo -e "${BLUE}Install Dashboard${NC}"
+kubectl create secret generic kubernetes-dashboard-certs --from-file=$HOME/certs/dashboard -n kube-system
+kubectl create -f manifests/dashboard-service.yaml
 kubectl create -f manifests/dashboard-user.yaml
 kubectl create -f manifests/namespaces.yaml
-
-# copy certificates
-cp certs/*.crt /usr/local/share/ca-certificates
-update-ca-certificates
-systemctl restart docker.service
 
 # install docker registry
 echo -e "${BLUE}Install registry${NC}"
@@ -23,7 +24,12 @@ docker run --entrypoint htpasswd registry:2 -Bbn kube-registry-user yf-ujhirt-cb
 chown root:root ./auth/htpasswd
 kubectl create -f manifests/docker-registry-daemonset.yaml
 
-# Connect k8s to registry
+# connect k8s to registry
 kubectl create secret docker-registry regcred --docker-server=10.10.112.27:5000 --docker-username=kube-registry-user --docker-password=yf-ujhirt-cbltk-rjhjkm --docker-email=demius.md@gmail.com
+
+# find bearer token for login to dashboard
+echo ""
+echo -e "${RED}COPY DASHBOARD AUTH TOKEN:${NC}"
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep dashboard-user | awk '{print $1}')
 
 echo -e "${RED}OK${NC}"
